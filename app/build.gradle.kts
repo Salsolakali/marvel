e74e1c89414b16dbd4a458ce01f9094d3c1a12f1
Alberto Fernandez Seral <alberto.fernandez@mo2o.com>
2021-05-26 12:37:22 +0200

    fix: updated appCompat version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,7 +46,7 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.4.2")
 
     // Support
-    implementation("androidx.appcompat:appcompat:1.2.0")
+    implementation("androidx.appcompat:appcompat:1.3.0")
     implementation("com.google.android.material:material:1.3.0")
 
     // ConstraintLayout
