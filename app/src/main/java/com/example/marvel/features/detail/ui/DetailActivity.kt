b957f194a166ad8308c232960fedd1269937dba0
Alberto Fernandez Seral <alberto.fernandez@mo2o.com>
2021-05-27 09:46:17 +0200

    feat: show details in detail activity

--- app/src/main/java/com/example/marvel/features/detail/ui/DetailActivity.kt
+++ app/src/main/java/com/example/marvel/features/detail/ui/DetailActivity.kt
@@ -4,7 +4,9 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.marvel.core.extensions.loadFromUrl
 import com.example.marvel.databinding.ActivityDetailBinding
+import com.example.marvel.features.home.domain.model.Character
 
 class DetailActivity : AppCompatActivity() {
 
@@ -19,8 +21,19 @@ class DetailActivity : AppCompatActivity() {
         }
     }
 
+    private fun getDetails() = intent.extras?.getSerializable(CHARACTER_KEY) as Character?
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_detail)
+        binding = ActivityDetailBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        initUI(getDetails())
+    }
+
+    private fun initUI(character: Character?) {
+        binding.imageDetail.loadFromUrl(character?.image ?: "")
+        binding.tvName.text = character?.name
+        binding.tvDescription.text = character?.description
     }
 }
